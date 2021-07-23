18353d2d91c894b8b2f6e462cc696c5f17e2c6bb
Lewis Maina <muriukimainalewis@gmail.com>
2021-07-23 12:42:16 +0300

    Updated config.py file

--- app.py
+++ app.py
@@ -11,15 +11,11 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # app configurations
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['UPLOAD_PATH'] = os.getenv("UPLOAD_PATH")
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png']
-app.config.from_object("config")
+app.config.from_object("config.DevelopmentConfig")
 
 # initialize the db
 db = SQLAlchemy(app)
+
 
 # Models
 class User(db.Model):
@@ -29,6 +25,7 @@ class User(db.Model):
     admin = db.Column(db.Boolean)
     created_at = db.Column(db.String())
 
+
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     profile_photo = db.Column(db.String(80))
@@ -37,70 +34,61 @@ class Profile(db.Model):
     lastname = db.Column(db.String(50))
     dob = db.Column(db.String(10))
     gender = db.Column(db.String(10))
-    phonenumber = db.Column(db.Integer, unique=True ,nullable=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'), unique=True)
-    user = db.relationship('User', backref=db.backref('profile', cascade="all, delete-orphan"),lazy='joined')
+    phonenumber = db.Column(db.Integer, unique=True, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    user = db.relationship('User', backref=db.backref('profile', cascade="all, delete-orphan"), lazy='joined')
 
 
 # Create a new user
-@app.route('/user/register', methods = ['POST'])
+@app.route('/user/register', methods=['POST'])
 def create_user():
     data = request.form
     hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(id=str(uuid.uuid4()), email=data['email'], password=hashed_password, admin=False, created_at=datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    new_user = User(id=str(uuid.uuid4()), email=data['email'], password=hashed_password, admin=False,
+                    created_at=datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"message": "User created"})
 
+
 # Login user
-@app.route('/user/login', methods = ['POST'])
+@app.route('/user/login', methods=['POST'])
 def login_user():
     data = request.form
     user = User.query.filter_by(email=data['email']).first()
     if not user:
         return jsonify({"message": "User not found"})
     if check_password_hash(user.password, data['password']):
-        user_data = {}
-        user_data['user_id'] = user.user_id 
-        user_data['email'] = user.email 
-        user_data['password'] = user.password 
-        user_data['admin'] = user.admin 
-        user_data['created_at'] = user.created_at 
+        user_data = {'user_id': user.user_id, 'email': user.email, 'password': user.password, 'admin': user.admin,
+                     'created_at': user.created_at}
         return jsonify({"user": user_data})
     return jsonify({"message": "Invalid email or password!"})
 
+
 # get all users in db
-@app.route('/user', methods = ['GET'])
+@app.route('/user', methods=['GET'])
 def get_all_users():
     users = User.query.all()
     output = []
     for user in users:
-        user_data = {}
-        user_data['user_id'] = user.id 
-        user_data['email'] = user.email 
-        user_data['password'] = user.password 
-        user_data['admin'] = user.admin 
-        user_data['created_at'] = user.created_at 
+        user_data = {'user_id': user.id, 'email': user.email, 'password': user.password, 'admin': user.admin,
+                     'created_at': user.created_at}
         output.append(user_data)
 
     return jsonify({"users": output})
 
+
 # get one user from db with id supplied
-@app.route('/user/<user_id>', methods = ['GET'])
+@app.route('/user/<user_id>', methods=['GET'])
 def get_one_user(user_id):
     user = User.query.filter_by(id=user_id).first()
-    user_data = {}
-    user_data['user_id'] = user.user_id 
-    user_data['email'] = user.email 
-    user_data['password'] = user.password 
-    user_data['admin'] = user.admin 
-    user_data['created_at'] = user.created_at 
-    
-
+    user_data = {'user_id': user.user_id, 'email': user.email, 'password': user.password, 'admin': user.admin,
+                 'created_at': user.created_at}
     return jsonify({"user": user_data})
 
+
 # delete user with id supplied from db
-@app.route('/user/<user_id>', methods = ['DELETE'])
+@app.route('/user/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     if not user:
@@ -112,7 +100,7 @@ def delete_user(user_id):
 
 
 # Create users profile
-@app.route('/profile', methods = ['POST'])
+@app.route('/profile', methods=['POST'])
 def create_user_profile():
     data = request.form
     user = User.query.filter_by(id=data['user_id']).first()
@@ -132,71 +120,61 @@ def create_user_profile():
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
             filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
             profile.profile_photo = filepath
-    
-    user_profile = Profile( username=data['username'], 
-                            profile_photo = filepath,
-                            firstname=data['firstname'],
-                            lastname=data['lastname'], 
-                            dob=data['dob'],
-                            gender=data['gender'],
-                            phonenumber=data['phonenumber'],
-                            user_id = data['user_id']
-                            )
-    
+
+    user_profile = Profile(username=data['username'],
+                           profile_photo=filepath,
+                           firstname=data['firstname'],
+                           lastname=data['lastname'],
+                           dob=data['dob'],
+                           gender=data['gender'],
+                           phonenumber=data['phonenumber'],
+                           user_id=data['user_id']
+                           )
+
     db.session.add(user_profile)
     db.session.commit()
     return jsonify({"message": "User Profile created"})
 
+
 # get all profiles in db
-@app.route('/profile', methods = ['GET'])
+@app.route('/profile', methods=['GET'])
 def get_all_profiles():
     profiles = Profile.query.all()
     output = []
     for profile in profiles:
-        profile_data = {}
-        profile_data['user_id'] = profile.user_id 
-        profile_data['phonenumber'] = profile.phonenumber 
-        profile_data['gender'] = profile.gender 
-        profile_data['dob'] = profile.dob 
-        profile_data['firstname'] = profile.firstname 
-        profile_data['lastname'] = profile.lastname 
-        profile_data['username'] = profile.username 
-        profile_data['profile_photo'] = profile.profile_photo 
+        profile_data = {'user_id': profile.user_id, 'phonenumber': profile.phonenumber, 'gender': profile.gender,
+                        'dob': profile.dob, 'firstname': profile.firstname, 'lastname': profile.lastname,
+                        'username': profile.username, 'profile_photo': profile.profile_photo}
         output.append(profile_data)
 
     return jsonify({"profiles": output})
 
+
 # get one profile from db
-@app.route('/profile/<user_id>', methods = ['GET'])
+@app.route('/profile/<user_id>', methods=['GET'])
 def get_user_profile(user_id):
     profile = Profile.query.filter_by(user_id=user_id).first()
     if not profile:
         abort(404)
-    profile_data = {}
-    profile_data['id'] = profile.id
-    profile_data['user_id'] = profile.user_id
-    profile_data['phonenumber'] = profile.phonenumber 
-    profile_data['gender'] = profile.gender 
-    profile_data['dob'] = profile.dob 
-    profile_data['firstname'] = profile.firstname 
-    profile_data['lastname'] = profile.lastname 
-    profile_data['username'] = profile.username 
-    profile_data['profile_photo'] = profile.profile_photo 
+    profile_data = {'id': profile.id, 'user_id': profile.user_id, 'phonenumber': profile.phonenumber,
+                    'gender': profile.gender, 'dob': profile.dob, 'firstname': profile.firstname,
+                    'lastname': profile.lastname, 'username': profile.username, 'profile_photo': profile.profile_photo}
 
     return jsonify({"profile": profile_data})
 
+
 # update profile in db
-@app.route('/profile/<profile_id>', methods = ['PUT'])
+@app.route('/profile/<profile_id>', methods=['PUT'])
 def update_profile(profile_id):
     data = request.form
     user = User.query.filter_by(id=data['user_id']).first()
     if not user:
         return jsonify({"message": "no user found"})
-    
+
     profile = Profile.query.filter_by(id=profile_id).first()
     if not profile:
         abort(404)
-    
+
     # check if profile photo is changed
     if 'file' in request.files:
         uploaded_file = request.files['file']
@@ -209,7 +187,7 @@ def update_profile(profile_id):
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
             filepath = os.path.join(app.config['UPLOAD_PATH'], filename)
             profile.profile_photo = filepath
-    
+
     profile.username = data['username']
     profile.firstname = data['firstname']
     profile.lastname = data['lastname']
@@ -218,8 +196,7 @@ def update_profile(profile_id):
     profile.phonenumber = data['phonenumber']
     db.session.commit()
 
-    return jsonify({"message":"Profile has been updated"})
-
+    return jsonify({"message": "Profile has been updated"})
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,4 +1,23 @@
 # config.py
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "Th1s%#1s&my*!S3cr3t%&K3y"
+    UPLOAD_PATH = "app/static/profile_photos/"
+    DATABASE_URL = 'sqlite:///app/db/prometheus.db'
+    UPLOAD_EXTENSIONS = ['.jpg', '.png']
+    MAX_CONTENT_LENGTH = 1024 * 1024
 
-# Enable Flask's debugging features. Should be False in production
-DEBUG = True
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    # Enable Flask's debugging features. Should be False in production
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
