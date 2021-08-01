2a8df4751ddd3c489005ca521182178d35733f54
Lewis Maina <muriukimainalewis@gmail.com>
2021-08-01 03:50:47 +0300

    Added update routes

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__, instance_relative_config=True)
 
 # allows us to make requests from one website to another website in the browser 
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "http://localhost:3000"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "https://prometheus-profile.herokuapp.com/"}})
 
 # load the views
 from app import views
