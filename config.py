ec1fa815aa51baecef4207248e1e9b6e820e0b33
Lewis Maina <muriukimainalewis@gmail.com>
2021-08-03 19:56:07 +0300

    with tests

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = decouple.config('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_EXTENSIONS = ['data:image/png;base64', 'data:image/jpg;base64', 'data:image/jpeg;base64']
     MAX_CONTENT_LENGTH = 1024 * 1024
     
 
 class ProductionConfig(Config):
-    db_path = os.path.abspath(os.getcwd()) + os.environ.get('DATABASE_URL')
+    db_path = os.path.abspath(os.getcwd()) + decouple.config('DATABASE_URL')
     db_uri = 'sqlite:///{}'.format(db_path)
     SQLALCHEMY_DATABASE_URI = db_uri
     UPLOAD_PATH = os.path.dirname(os.getcwd()) + "/app/static/images/"
@@ -21,7 +21,7 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    db_path = os.path.abspath(os.getcwd()) + os.environ.get('DEVELOPMENT_DATABASE_URL')
+    db_path = os.path.abspath(os.getcwd()) + decouple.config('DEVELOPMENT_DATABASE_URL')
     db_uri = 'sqlite:///{}'.format(db_path)
     SQLALCHEMY_DATABASE_URI = db_uri
 
@@ -30,5 +30,5 @@ class TestingConfig(Config):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite://"
-    UPLOAD_PATH = os.path.dirname(os.getcwd()) + "/app/static/test_images/"
+    UPLOAD_PATH = os.path.abspath(os.getcwd()) + "/app/static/test_images/"
 
