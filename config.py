a9acd4931d6675a0f3133c615d59e120a7c46411
Lewis Maina <muriukimainalewis@gmail.com>
2021-08-03 21:09:46 +0300

    Addded path

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     SECRET_KEY = decouple.config('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_PATH = "app/static/images/"
+    UPLOAD_PATH = "/app/static/images/"
     UPLOAD_EXTENSIONS = ['data:image/png;base64', 'data:image/jpg;base64', 'data:image/jpeg;base64']
     MAX_CONTENT_LENGTH = 1024 * 1024
     
